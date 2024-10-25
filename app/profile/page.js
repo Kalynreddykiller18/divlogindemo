a81784cb7b3454ba9063ca0f13cb1dc7856e1279
unknown <kalyanbejjanki9010@gmail.com>
2024-10-25 11:37:27 +0530

    UI Fixed 2

--- app/profile/page.js
+++ app/profile/page.js
@@ -12,10 +12,10 @@ export default function Profile() {
       <h1 style={{color:"red",fontSize:"1.89rem",margin:"10px 0"}}>Profile Page</h1>
       {user ? (
         <div className='profile'>
-          <div className='profli'><span>Name:</span> <span>{user.name}</span></div>
-          <div className='profli'><span>Mobile No:</span> <span>{user.mobile_no}</span></div>
-          <div className='profli'><span>WhatsApp No:</span> <span>{user.whatsapp_no }</span></div>
-          <div className='profli'>Email: <span>{user.email ? user.email : <>Mail id not exits</>}</span></div>
+          <div className='profli'><span>Name</span> <span>{user.name}</span></div>
+          <div className='profli'><span>Mobile No</span> <span>{user.mobile_no}</span></div>
+          <div className='profli'><span>WhatsApp No</span> <span>{user.whatsapp_no }</span></div>
+          <div className='profli'>Email <span>{user.email ? user.email : <>Mail id not exits</>}</span></div>
           <button><Link href='/updateprofile'>Update Profile</Link></button>
         </div>
       ) : (
